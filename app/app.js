869cf60c0182933cf19452078f33d1d5da3f9557
Siraj <sirajchokshi@gmail.com>
2020-05-01 14:17:24 -0400

    declare vendors, update UI, add README in doc

--- app/app.js
+++ app/app.js
@@ -28,7 +28,30 @@ var ctx = c.getContext("2d");
 
 const WATER_LEVEL = 2, COMPLEXITY = 25, RESOLUTION = 100
 
-let SCALE = 1, X_POS = 0, Y_POS = 0, arr = Array(RESOLUTION).fill().map(() => Array(RESOLUTION).fill(0));
+let SCALE = 1, X_POS = 0, Y_POS = 0, arr = Array(RESOLUTION).fill().map(() => Array(RESOLUTION).fill(0)), SEED = Math.floor(Math.random() * 1000000);
+document.getElementById("enter-seed").value = SEED;
+
+const hashCode = (s) => {
+    let h;
+    for(let i = 0; i < s.length; i++) 
+          h = Math.imul(31, h) + s.charCodeAt(i) | 0;
+    return Math.abs(h);
+}
+
+const randomSeed = () => {
+  SEED = Math.floor(Math.random() * 1000000);
+  document.getElementById("enter-seed").value = SEED;
+  paintMap();
+}
+
+const loadSeed = () => {
+  const seedIn = document.getElementById("enter-seed").value;
+  if (isNaN(seedIn) || seedIn < 0) {
+      SEED = hashCode(document.getElementById("enter-seed").value);
+  } else SEED = seedIn
+  console.log(SEED)
+  paintMap();
+}
 
 const mag = () => {
   if (SCALE >= 0.5) SCALE -= 0.1;
@@ -40,6 +63,43 @@ const min = () => {
   paintMap();
 }
 
+const isPosValid = () => {
+  if (document.getElementById('x-pos').value < 0 || document.getElementById('y-pos').value < 0) {
+    document.getElementById('go-to-btn').disabled = true;
+  } else document.getElementById('go-to-btn').disabled= false;
+}
+
+const goTo = () => {
+  const x = document.getElementById("x-pos").value
+  const y = document.getElementById('y-pos').value
+  if (x >= 0 && y >= 0) {
+      X_POS = x * 10;
+      Y_POS = y * 10;
+  }
+  paintMap();
+}
+
+const disableDir = (dir) => {
+  const btns = document.getElementsByClassName(dir);
+  for (let btn of btns) {
+    btn.disabled = true; 
+  }
+}
+
+const enableDir = (dir) => {
+  const btns = document.getElementsByClassName(dir);
+  for (let btn of btns) {
+    btn.disabled = false; 
+  }
+}
+
+const checkDir = () => {
+  enableDir('up')
+  enableDir('left')
+  if (X_POS - 10 < 0) disableDir('left')
+  if (Y_POS - 10 < 0) disableDir('up')
+}
+
 const move = (vector) => {
   if (X_POS + vector[0] * 5 >= 0) X_POS += vector[0] * 5;
   if (Y_POS + vector[1] * 5 >= 0) Y_POS += vector[1] * 5;
@@ -49,6 +109,7 @@ const move = (vector) => {
 const paintMap = () => {
   let scaler = 600 / RESOLUTION
   console.time("paintMap");
+  
   fetch('/bin/main.wasm').then(response =>
     response.arrayBuffer()
   ).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
@@ -57,21 +118,32 @@ const paintMap = () => {
       for (let y = 0; y < arr[0].length; y++) {
         nx = ((x + X_POS)/arr.length) * SCALE * 0.3
         ny = ((y + Y_POS)/arr[0].length) * SCALE * 0.3
-        arr[x][y] = Math.floor(instance.exports.perlin2d(nx, ny, 10, 19, 0) * COMPLEXITY - 12);
+        arr[x][y] = Math.floor(instance.exports.perlin2d(nx, ny, 10, 19, SEED) * COMPLEXITY - 12);
         ctx.beginPath();
         ctx.rect(scaler * x, scaler * y, scaler, scaler);
         if (arr[x][y] < WATER_LEVEL) ctx.fillStyle = "#3399ff";
-        else if (arr[x][y] < WATER_LEVEL + 2 && arr[x][y] > WATER_LEVEL - 1) ctx.fillStyle = "#ffe6b3";
+        else if (arr[x][y] < WATER_LEVEL + 2 && arr[x][y] > WATER_LEVEL - 1) {
+          ctx.fillStyle = "#ffe6b3";
+          if (Math.random() > 0.4) {
+            ctx.fillStyle = pSBC(0.02, ctx.fillStyle)
+          }
+        }
         else if (arr[x][y] < WATER_LEVEL + 6) ctx.fillStyle = "#5cd65c";
-        else if (arr[x][y] < WATER_LEVEL + 7) ctx.fillStyle = "#009933";
-        else if (arr[x][y] < WATER_LEVEL + 11) ctx.fillStyle = "#85adad";
+        else if (arr[x][y] < WATER_LEVEL + 7) ctx.fillStyle = "#6e9668";
+        else if (arr[x][y] < WATER_LEVEL + 9) ctx.fillStyle = "#85adad";
         else ctx.fillStyle = "#eff5f5";
         let difference = arr[x][y] - WATER_LEVEL;
         if (difference < -7) difference = 7;
-        ctx.fillStyle = pSBC(Math.abs(difference) * -0.04, ctx.fillStyle)
+        
+        let shade = -0.04;
+        if (arr[x][y] >= WATER_LEVEL) {
+          shade = -0.08
+        }
+        ctx.fillStyle = pSBC(Math.abs(difference) * shade, ctx.fillStyle)
         if (Math.random() > 0.7) {
           ctx.fillStyle = pSBC(0.012, ctx.fillStyle)
         }
+        
         ctx.fill();
       }
     }
@@ -89,9 +161,11 @@ const paintMap = () => {
     ctx.rect(scaler * (arr.length/2 + 0.25), scaler * (arr[0].length/2 - 1), scaler * 0.5, scaler * 3);
     ctx.fill();
     
-    document.getElementById('x-pos').innerText = X_POS / 10
-    document.getElementById('y-pos').innerText = Y_POS / 10
+    checkDir();
+    document.getElementById('x-pos').value = X_POS / 10
+    document.getElementById('y-pos').value = Y_POS / 10
     
     console.timeEnd("paintMap");
+    // console.log(Math.max(...arr.flat(1)))
   }).catch(console.error);
 }

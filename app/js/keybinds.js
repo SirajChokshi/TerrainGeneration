e721492ec24f502b1fc387058e4f40dc63af5057
Siraj <sirajchokshi@gmail.com>
2020-05-05 16:58:28 -0400

    add keybind escape for Input elements

--- app/js/keybinds.js
+++ app/js/keybinds.js
@@ -1,17 +1,18 @@
 document.addEventListener('keydown', (e) => {
-	switch (e.key) {
-		case "w":
-			move([0,-2])
-			break;
-		case "s":
-			move([0,2])
-			break;
-		case "a":
-			move([-2,0])
-			break;
-		case "d":
-			move([2,0])
-			break;
-		default:
-			break;
-}})
+	if (e.target.tagName != "INPUT") {
+		switch (e.key) {
+			case "w":
+				move([0,-2])
+				break;
+			case "s":
+				move([0,2])
+				break;
+			case "a":
+				move([-2,0])
+				break;
+			case "d":
+				move([2,0])
+				break;
+			default:
+				break;
+}}})
